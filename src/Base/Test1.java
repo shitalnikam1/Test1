9c9767a8381102b1e0b750b874a6c9179ee4f9ea
Shital <Shital@LAPTOP-9PVB74IQ>
2022-10-02 16:51:44 +0530

    commit

---
+++ src/Base/Test1.java
@@ -0,0 +1,16 @@
+package Base;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Good mOrning");
+		m1();
+		
+	}
+	public static void m1()
+	{
+		System.out.println("shital");
+	}
+
+}
